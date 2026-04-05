a5cb23f15f8fa8d1090b5d7c144a6e96ff9093b1
dgSloxx417 <donkorgerald0@gmail.com>
2026-04-05 21:53:13 +0000

    Implement animated count

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
